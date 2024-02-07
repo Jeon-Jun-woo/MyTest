e40ee7398079b0f4e6f3eb2c40de86e0a3b9e1db
BANN <BANN@bbanggi>
2024-02-07 09:34:11 +0900

    기현

--- TestProject/src/main/java/com/sist/web/HomeController.java
+++ TestProject/src/main/java/com/sist/web/HomeController.java
@@ -36,7 +36,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("hihihihihihihihihih");
 		return "home";
 	}
 	
